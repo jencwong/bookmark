59a0f4763f7dbc1e36ba176d28a2459347a7deb5
Jenwng <Jenniescwong@gmail.com>
2019-11-20 07:29:46 -0500

    add css bulmark links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Main from "./components/Main.js";
 import "./App.css";
+import "react-bulma-components/dist/react-bulma-components.min.css";
+import { Button } from "react-bulma-components";
 
 function App() {
   return (
